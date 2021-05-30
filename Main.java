53fead1ca6bff096b9ac3a33e7fee1b5bf3efeb5
Amarmalic0 <81154113+Amarmalic0@users.noreply.github.com>
2021-05-30 17:24:32 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        static void  ABC(){
+            char [] abc = new char[26];
+            char i = 'A';
+            for (int a = 0; a <= 26; a++) {
+                System.out.println(i);
+                i++;
+            }
+        }
+    }
+}
